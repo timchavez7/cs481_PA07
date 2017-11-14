696b2d1117593c5517a45d7dd43f53d5941b6734
timchavez7 <tchavez22@unm.edu>
2017-11-14 14:55:41 -0700

    I fixed the magic. You're welcome!

--- PA07magic_BOGE_CHTI.c
+++ PA07magic_BOGE_CHTI.c
@@ -4,10 +4,6 @@
 int main(int argc, char **argv) {
     FILE *filepointer;
     unsigned char buffer[8];
-    // unsigned char opcode[4] = {0};
-    // unsigned char arg1[4] = {0};
-    // unsigned char arg2[4] = {0};
-    // unsigned char arg3[4] = {0};
 
     if (argc < 2) {
         printf("ERROR: need filename\n");
@@ -20,10 +16,7 @@ int main(int argc, char **argv) {
     }
 
     fread(buffer, 1, 4, filepointer);
-    if (buffer[0] == 0xff && buffer[1] == 0x00 && buffer[2] == 0xff && buffer[3] == 0xe0) {
-        printf("JPG\n");
-    }
-    else if (buffer[0] == 0x7f && buffer[1] == 0x45 && buffer[2] == 0x4c && buffer[3] == 0x46) {
+    if (buffer[0] == 0x7f && buffer[1] == 0x45 && buffer[2] == 0x4c && buffer[3] == 0x46) {
         printf("ELF\n");
     }
     else if (buffer[0] == 0x25 && buffer[1] == 0x50 && buffer[2] == 0x44 && buffer[3] == 0x46) {
@@ -31,6 +24,9 @@ int main(int argc, char **argv) {
     }
     else if (buffer[0] == 0x23 && buffer[1] == 0x21) {
         printf("Linux script\n");
+    }
+    else if (buffer[0] == 0xff && buffer[1] == 0xd8) {
+        printf("JPG\n");
     }
     else {
         if (fseek(filepointer, 257, SEEK_SET) != 0) {
